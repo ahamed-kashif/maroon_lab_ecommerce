3bd577d54d3976d873d817329a085da2700d3e43
ahamed-kashif <ahamed.kashif@northsouth.edu>
2020-09-16 07:39:18 +0600

    super-admin is added in seeder.

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Add admin
+        $user = User::create(
+            [
+                'id' 		=> 	1,
+                'name'	=>	'Mr. Admin',
+                'email'		=>	'admin@maroon.lab',
+                'mobile_number' => '01783511730',
+                'password'	=>	Hash::make('12345678')
+            ]
+        );
+        $role = Role::create(['name' => 'super-admin']);
+        $role->givePermissionTo(Permission::all());
+
+        $admin_user = User::find(0);
+        $admin_user->assignRole('super-admin');
+    }
+}
