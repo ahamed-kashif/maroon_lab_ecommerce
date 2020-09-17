744d2445ae693a008dc9b6647ff7508bac76761c
ahamed-kashif <ahamed.kashif@northsouth.edu>
2020-09-18 04:41:45 +0600

    back-end database initiated

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Product;
+use Faker\Generator as Faker;
+
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'title' => $faker->jobTitle,
+        'price' => $faker->numberBetween(500,10000),
+
+    ];
+});
