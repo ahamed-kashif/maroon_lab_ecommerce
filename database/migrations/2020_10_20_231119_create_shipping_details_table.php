790c360371dc7a7a7b8ebdc5ec0e3295bfceac5e
ahamed-kashif <ahamed.kashif@northsouth.edu>
2020-10-21 05:25:44 +0600

    shipping details done

---
+++ database/migrations/2020_10_20_231119_create_shipping_details_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShippingDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shipping_details', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('contact_number');
+            $table->string('billing_address');
+            $table->string('city');
+            $table->string('post_code');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shipping_details');
+    }
+}
