8d41ef980589932cad65c0d03e3a25844f666e1d
ahamed-kashif <ahamed.kashif@northsouth.edu>
2020-10-25 04:23:03 +0600

    typo

--- resources/views/partials/paritals-customer-invoice.blade.php
+++ resources/views/partials/paritals-customer-invoice.blade.php
@@ -4,17 +4,13 @@
             <div class="col-7">
                 <h5 class="card-title mb-0">Invoice PDF Details</h5>
             </div>
-            <div class="col-5 text-right">
-                <a href="{{route('customer.order.invoice', $order->id)}}" class="btn btn-success py-1"><i class="feather icon-file mr-2"></i>Invoice</a>
-            </div>
         </div>
     </div>
     <div class="card-body">
         <div class="order-primary-detail">
-            <h6>Current PDF Details</h6>
-            <p class="mb-0">Order Code : #{{strtoupper($order->code)}}</p>
-            <p class="mb-0">Transaction Code : {{strtoupper($order->transaction->code)}}</p>
-            <p class="mb-0">Tracking : {{strtoupper($order->order_tracking->id)}}</p>
+            <div class="text-right">
+                <a href="{{route('customer.order.invoice', $order->id)}}" class="btn btn-success py-1 float-right"><i class="feather icon-file mr-2"></i>Invoice</a>
+            </div>
         </div>
     </div>
 </div>
