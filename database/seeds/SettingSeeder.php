a9696cc0e322fc1019de4b4faec1c91ecb7e41f8
ahamed-kashif <ahamed.kashif@northsouth.edu>
2020-10-29 04:49:53 +0600

    setting unresolved

---
+++ database/seeds/SettingSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class SettingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Permission::create(['name' => 'edit setting']);
+        Permission::create(['name' => 'delete setting']);
+        Permission::create(['name' => 'create setting']);
+        Permission::create(['name' => 'index setting']);
+        Permission::create(['name' => 'show setting']);
+        Permission::create(['name' => 'update setting']);
+        Permission::create(['name' => 'api_get setting']);
+        Role::findByName('super-admin')->givePermissionTo(Permission::all());
+        Role::findByName('admin')->givePermissionTo('show setting');
+    }
+}
