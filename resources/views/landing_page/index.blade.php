62964dd4dd4bfa47c1f4a07ad5c18bd1cb4099a8
ahamed-kashif <ahamed.kashif@northsouth.edu>
2020-10-29 06:35:27 +0600

    double banner resolved

--- resources/views/landing_page/index.blade.php
+++ resources/views/landing_page/index.blade.php
@@ -101,36 +101,7 @@
 <!--=============================================
 =            Double banner          =
 =============================================-->
-
-<div class="double-banner-section mb-35">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-12 mb-xs-35">
-                <!--=======  single banner  =======-->
-
-                <div class="single-banner">
-                    <a href="shop-left-sidebar.html">
-                        <img src="assets/images/banners/home2-banner1-1.jpg" class="img-fluid" alt="">
-                    </a>
-                </div>
-
-                <!--=======  End of single banner  =======-->
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-12">
-                <!--=======  single banner  =======-->
-
-                <div class="single-banner">
-                    <a href="shop-left-sidebar.html">
-                        <img src="assets/images/banners/home2-banner1-2.jpg" class="img-fluid" alt="">
-                    </a>
-                </div>
-
-                <!--=======  End of single banner  =======-->
-            </div>
-        </div>
-    </div>
-</div>
-
+@include('landing_page.partials.double-banner')
 <!--=====  End of Double banner   ======-->
 
 <!--=============================================
---
+++ resources/views/landing_page/partials/double-banner.blade.php
@@ -0,0 +1,24 @@
+<div class="double-banner-section mb-35">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-12 mb-xs-35">
+                <!--=======  single banner  =======-->
+                <div class="single-banner">
+                    <a href="#">
+                        <img src="{{asset('landing_page/images/banners/fb.gif')}}" class="img-fluid" alt="">
+                    </a>
+                </div>
+                <!--=======  End of single banner  =======-->
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12">
+                <!--=======  single banner  =======-->
+                <div class="single-banner">
+                    <a href="#">
+                        <img src="{{asset('landing_page/images/banners/whatsapp.jpg')}}" class="img-fluid" alt="">
+                    </a>
+                </div>
+                <!--=======  End of single banner  =======-->
+            </div>
+        </div>
+    </div>
+</div>
