43585cd7c63d8d07ae431578df37502b5c6a4fa0
ahamed-kashif <ahamed.kashif@northsouth.edu>
2020-10-31 19:02:36 +0600

    queue scheduler added

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,9 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('queue:work --tries=3')
+            ->cron('* * * * * *')
+            ->withoutOverlapping();
     }
 
     /**
