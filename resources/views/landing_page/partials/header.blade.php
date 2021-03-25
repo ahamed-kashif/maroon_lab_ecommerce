207f3f69a172b0b0fd2c7b4d22ab5ec6f792cf53
Kashif <ahamed.kashif@northsouth.edu>
2021-03-25 17:40:47 +0600

    resolved header hero, unresolved all

---
+++ resources/views/landing_page/partials/header.blade.php
@@ -0,0 +1,129 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="container">
+                <div class="row">
+                    <div class="header-banner">
+                        <div class="assetBlock">
+                            <div id="slideshow">
+                                <p>Special Offers! - Get <span>50%</span> off on vegetables </p>
+                                <p>sale <span>40%</span> of  on bulk shopping! </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="header">
+        <div class="container">
+            <div class="header-container row">
+                <div class="logo">
+                    <a href="/" title="index">
+                        <div><img src="{{smallLogoUrl()}}" alt="logo" width="100" height="50"></div>
+                    </a>
+                </div>
+                <div class="fl-nav-menu">
+                    <nav>
+                        <div class="mm-toggle-wrap">
+                            <div class="mm-toggle"><i class="icon-align-justify"></i><span class="mm-label">Menu</span> </div>
+                        </div>
+                        <div class="nav-inner">
+                            <!-- BEGIN NAV -->
+                            <ul id="nav" class="hidden-xs">
+
+                                <li> <a class="level-top" href="#"><span>Home</span></a></li>
+                                @foreach(categories() as $category)
+                                    <li class="level0 parent drop-menu"><a href="{{route('store.category'),$category->id}}"><span>{{ucfirst($category->title)}}</span> </a>
+                                        <!--sub sub category-->
+                                        @if (count($category->subcategories) > 0)
+                                            <ul class="level1">
+                                                @foreach($category->subcategories as $sub)
+                                                <li class="level1 first"><a href="{{route('store.subcategory',$sub->id)}}"><span>{{ucfirst($sub->title)}}</span></a></li>
+                                                @endforeach
+                                            </ul>
+                                        @endif
+                                    </li>
+                                @endforeach
+                            </ul>
+                            <!--nav-->
+                        </div>
+                    </nav>
+                </div>
+
+                <!--row-->
+
+                <div class="fl-header-right">
+                    <div class="fl-links">
+                        <div class="no-js">
+                            <a title="Company" class="clicker"></a>
+                            <div class="fl-nav-links">
+                                <ul class="links">
+                                    <li><a href="{{route('home')}}" title="My Account">My Account</a></li>
+                                    <li><a href="{{route('checkout.create')}}" title="Checkout">Checkout</a></li>
+{{--                                    <li class="last"><a href="login.html" title="Login"><span>Login</span></a></li>--}}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="fl-cart-contain">
+                        <div class="mini-cart">
+                            <div class="basket">
+                                <a href="{{route('cart.index')}}"><span> {{session()->has('cart') ? count(session()->get('cart')->items()) : '0'}} </span></a>
+                            </div>
+                            <div class="fl-mini-cart-content" style="display: none;">
+                                <div class="block-subtitle">
+                                    <div class="top-subtotal">{{session()->has('cart') ? count(session()->get('cart')->items()) : '0'}} items, <span class="price">{{session()->has('cart') ? session()->get('cart')->bill() : '0'}}TK</span> </div>
+                                    <!--top-subtotal-->
+                                    <!--pull-right-->
+                                </div>
+                                <!--block-subtitle-->
+                                @if (session()->has('cart'))
+                                    @if (count(session()->get('cart')->items()) > 0)
+                                        <ul class="mini-products-list" id="cart-sidebar">
+                                            @foreach(session()->get('cart')->items() as $item)
+                                                <li class="item first">
+                                                    <div class="item-inner">
+                                                        <a class="product-image" title="{{$item->product()->title}}" href="{{route('store.product.show',$item->product()->id)}}">
+                                                            @if($item->product()->images->count() > 0)
+                                                                <img alt="{{$item->product()->title}}" src="{{$item->images->first()->url}}">
+                                                            @else
+                                                                <img alt="{{$item->product()->title}}" src="{{asset('images/ecommerce/no_image.png')}}">
+                                                            @endif
+                                                        </a>
+                                                        <div class="product-details">
+                                                            <!--access-->
+                                                            <strong>{{$item->getQty()}}</strong> x <span class="price">{{$item->amount()}} TK</span>
+                                                            <p class="product-name"><a href="{{route('store.product.show',$item->product()->id)}}">{{$item->product()->title}}</a></p>
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                            @endforeach
+                                        </ul>
+                                    @endif
+                                @endif
+                                <div class="actions">
+                                    <button class="btn-checkout" title="Checkout" type="button" onclick="window.location = {{route('checkout.create')}}"><span>Checkout</span></button>
+                                </div>
+                                <!--actions-->
+                            </div>
+                            <!--fl-mini-cart-content-->
+                        </div>
+                    </div>
+                    <!--mini-cart-->
+                    <div class="collapse navbar-collapse">
+                        <form class="navbar-form" role="search" action="{{route('search.index')}}" method="get">
+                            @csrf
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Search">
+                                <span class="input-group-btn">
+                  <button type="submit" class="search-btn"> <span class="glyphicon glyphicon-search"> <span class="sr-only">Search</span> </span> </button>
+                  </span> </div>
+                        </form>
+                    </div>
+                    <!--links-->
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ resources/views/landing_page/partials/hero.blade.php
@@ -0,0 +1,29 @@
+<div id="thmg-slider-slideshow" class="thmg-slider-slideshow">
+    <div class="container">
+        <div id='thm_slider_wrapper' class='thm_slider_wrapper fullwidthbanner-container' >
+            <div id='thm-rev-slider' class='rev_slider fullwidthabanner'>
+                <ul>
+                    <li data-transition='random' data-slotamount='7' data-masterspeed='1000' data-thumb='{{asset('landing_page/images/slide-img2.jpg')}}'>
+                        <img src='{{asset('landing_page/images/slide-img2.jpg')}}'  data-bgposition='left top'  data-bgfit='cover' data-bgrepeat='no-repeat' alt="slider-image1" />
+                        <div class="info">
+                            <div class='tp-caption ExtraLargeTitle sft  tp-resizeme ' data-x='0'  data-y='220'  data-endspeed='500'  data-speed='500' data-start='1100' data-easing='Linear.easeNone' data-splitin='none' data-splitout='none' data-elementdelay='0.1' data-endelementdelay='0.1' style='z-index:2; white-space:nowrap;'><span>Fresh Food</span></div>
+                            <div class='tp-caption LargeTitle sfl  tp-resizeme ' data-x='0'  data-y='300'  data-endspeed='500'  data-speed='500' data-start='1300' data-easing='Linear.easeNone' data-splitin='none' data-splitout='none' data-elementdelay='0.1' data-endelementdelay='0.1' style='z-index:3; white-space:nowrap;'>Simply <span>delicious</span></div>
+                            <div class='tp-caption sfb  tp-resizeme ' data-x='0'  data-y='520'  data-endspeed='500'  data-speed='500' data-start='1500' data-easing='Linear.easeNone' data-splitin='none' data-splitout='none' data-elementdelay='0.1' data-endelementdelay='0.1' style='z-index:4; white-space:nowrap;'><a href='#' class="buy-btn">Shop Now</a></div>
+                            <div    class='tp-caption Title sft  tp-resizeme ' data-x='0'  data-y='420'  data-endspeed='500'  data-speed='500' data-start='1500' data-easing='Power2.easeInOut' data-splitin='none' data-splitout='none' data-elementdelay='0.1' data-endelementdelay='0.1' style='z-index:4; white-space:nowrap;'>We supply highly quality organic products</div>
+                        </div>
+                    </li>
+                    <li data-transition='random' data-slotamount='7' data-masterspeed='1000' data-thumb='{{asset('landing_page/images/slide-img3.jpg')}}'>
+                        <img src='{{asset('landing_page/images/slide-img3.jpg')}}'  data-bgposition='left top'  data-bgfit='cover' data-bgrepeat='no-repeat' alt="slider-image2"  />
+                        <div class="info">
+                            <div class='tp-caption ExtraLargeTitle sft  tp-resizeme ' data-x='0'  data-y='220'  data-endspeed='500'  data-speed='500' data-start='1100' data-easing='Linear.easeNone' data-splitin='none' data-splitout='none' data-elementdelay='0.1' data-endelementdelay='0.1' style='z-index:2; white-space:nowrap;'><span>Fresh Look</span></div>
+                            <div class='tp-caption LargeTitle sfl  tp-resizeme ' data-x='0'  data-y='300'  data-endspeed='500'  data-speed='500' data-start='1300' data-easing='Linear.easeNone' data-splitin='none' data-splitout='none' data-elementdelay='0.1' data-endelementdelay='0.1' style='z-index:3; white-space:nowrap;'><span>100%</span> Organic</div>
+                            <div class='tp-caption sfb  tp-resizeme ' data-x='0'  data-y='520'  data-endspeed='500'  data-speed='500' data-start='1500' data-easing='Linear.easeNone' data-splitin='none' data-splitout='none' data-elementdelay='0.1' data-endelementdelay='0.1' style='z-index:4; white-space:nowrap;'><a href='#' class="buy-btn">Shop Now</a></div>
+                            <div class='tp-caption Title sft  tp-resizeme ' data-x='0'  data-y='420'  data-endspeed='500'  data-speed='500' data-start='1500' data-easing='Power2.easeInOut' data-splitin='none' data-splitout='none' data-elementdelay='0.1' data-endelementdelay='0.1' style='z-index:4; white-space:nowrap;'>Farm Fresh Produce Right to Your Door</div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
